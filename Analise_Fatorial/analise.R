bdfd4eaa4780c07a14a1beda46b5753cc610ce59
Kaique de Jesus Pessoa Santos <kaique10defevereiro@gmail.com>
2024-12-01 12:53:10 -0300

    Terminei a parte de KMO, matriz de correlação e teste de barlett

--- Analise_Fatorial/analise.R
+++ Analise_Fatorial/analise.R
@@ -1,22 +1,26 @@
-data <- read.csv("Analise_Fatorial\\Passageiros_Satisfacao_menor.csv"
-)
-
-
-#View(data)
-data$Class <- ifelse(data$Class == "Eco", 1, 0)
-data$Gender <- ifelse(data$Gender == "Male", 1, 0)
-data$satisfaction <- ifelse(data$satisfaction == "satisfied", 1, 0)
-selected_columns <- data[, c("Gender", "Class", "Age","Flight.Distance","Gate.location","satisfaction")] 
-#View(selected_columns)
-
-#correlation_matrix <- cor(selected_columns)
-#print(correlation_matrix)
-
 library(psych)
 
-selected_columns <- selected_columns[, -which(names(selected_columns) %in% c("Gender", "Gate.location"))]
-correlation_matrix <- cor(selected_columns)
-kmo_result <- KMO(correlation_matrix)
+
+data <- read.csv("Analise_Fatorial\\Passageiros_Satisfacao_menor.csv")
+#View(data)
+
+data$Class <- ifelse(data$Class == "Eco", 1, 0)
+#data$Gender <- ifelse(data$Gender == "Male", 1, 0)
+data$satisfaction <- ifelse(data$satisfaction == "satisfied", 1, 0)
+
+# "Gender" e "Gate.location" tem KMO muito baixo, então retirei essas variáveis
+selected_columns <- data[, c( "Class", "Age","Flight.Distance","satisfaction")] 
+#View(selected_columns)
+
+
+# KMO
+kmo_result <- KMO(selected_columns)
 print(kmo_result)
 
+# Matriz de Correlação
+correlation_matrix <- cor(selected_columns)
+print(correlation_matrix)
 
+# Teste de Bartlett
+bartlett_test <- cortest.bartlett(correlation_matrix, n = 10000)
+print(bartlett_test)
